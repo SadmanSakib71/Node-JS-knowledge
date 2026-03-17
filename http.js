ca98684d11258eb6ec95a27535de1caac72cc626
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-18 01:35:21 +0600

    running different route

--- http.js
+++ http.js
@@ -1,9 +1,17 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-  res.write("Hey who are you ");
-  res.write("My Name is sakib");
-  res.end();
+  if (req.url === "/") {
+    res.write("Hey who are you ");
+    res.write("My Name is sakib");
+    res.end();
+  } else if (req.url === "/about") {
+    res.write("This is about us page");
+    res.end();
+  } else {
+    res.write("nothing");
+    res.end();
+  }
 });
 
 server.listen(3000);
