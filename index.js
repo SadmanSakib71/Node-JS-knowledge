f5b511ac17f4789835ae5d64f684b6cd08db9449
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-18 06:23:52 +0600

    write stream learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require("node:fs");
 
 const OurReadSystem = fs.createReadStream(`${__dirname}/sakib.txt`);
+const OurWriteSystem = fs.createWriteStream(`${__dirname}/Output.txt`);
 
-OurReadSystem.on("data", (data) => {
-  console.log(data.toString());
+OurReadSystem.on("data", (chunk) => {
+  OurWriteSystem.write(chunk);
 });
