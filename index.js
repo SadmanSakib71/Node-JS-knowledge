baddfd1868945a94f8dc29dfe17576601af454db
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-18 06:42:41 +0600

    show request data in response

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const http = require("node:http");
 const fs = require("node:fs");
 
-const OurReadSystem = fs.createReadStream(`${__dirname}/sakib.txt`);
-const OurWriteSystem = fs.createWriteStream(`${__dirname}/Output.txt`);
-
-OurReadSystem.on("data", (chunk) => {
-  OurWriteSystem.write(chunk);
+const server = http.createServer((req, res) => {
+  const myReadStream = fs.createReadStream(`${__dirname}/sakib.txt`, "utf8");
+  myReadStream.pipe(res);
 });
+
+server.listen(3000);
+
+console.log("Run on 3000 port");
---
+++ readAndwrite.js
@@ -0,0 +1,10 @@
+const fs = require("node:fs");
+
+const OurReadSystem = fs.createReadStream(`${__dirname}/sakib.txt`);
+const OurWriteSystem = fs.createWriteStream(`${__dirname}/Output.txt`);
+
+// OurReadSystem.on("data", (chunk) => {
+//   OurWriteSystem.write(chunk);
+// });
+
+OurReadSystem.pipe(OurWriteSystem);
