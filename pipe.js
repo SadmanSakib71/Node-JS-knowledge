07f83af24a4b25f0dfdad9b9a284efc6c1d8d6e4
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-19 19:33:19 +0600

    pipe js

---
+++ pipe.js
@@ -0,0 +1,11 @@
+const http = require("node:http");
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+  const myReadStream = fs.createReadStream(`${__dirname}/sakib.txt`, "utf8");
+  myReadStream.pipe(res);
+});
+
+server.listen(3000);
+
+console.log("Run on 3000 port");
